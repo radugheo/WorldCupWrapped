50d092b54b8847ab405a1099a5c09e11bdb6b54c
Radu Mihai Gheorghe <radu2002ro@gmail.com>
2023-01-14 20:00:57 +0200

    test commit

--- WorldCupWrapped/Controllers/WeatherForecastController.cs
+++ WorldCupWrapped/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WorldCupWrapped.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Fierbinte", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
