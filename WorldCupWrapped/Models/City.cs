a3ae20967e2f57b6ce26fcf0ead7963b5909989a
Radu Mihai Gheorghe <radu2002ro@gmail.com>
2023-01-15 21:08:13 +0200

    added models

---
+++ WorldCupWrapped/Models/City.cs
@@ -0,0 +1,11 @@
+﻿using Lab3_14.Models.Base;
+
+namespace WorldCupWrapped.Models
+{
+    public class City : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public ICollection<Stadium> Stadiums { get; set; }
+    }
+}
