a4ca8f15f5f8a9e83f54fb01c244dbaab69c2408
DESKTOP-CVHSL6Q\stoic <elias-valeriu.stoica@s.unibuc.ro>
2023-02-07 20:57:31 +0200

    groups frontend

--- WorldCupWrapped/ClientApp/src/app/components/groups/groups.component.ts
+++ WorldCupWrapped/ClientApp/src/app/components/groups/groups.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AxiosResponse } from 'axios';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-groups',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
-  ngOnInit(): void {
+  groups: any;
+  matches: any;
+  selectedGroup: any;
+  showGroupMatches = false;
+
+  async ngOnInit(): Promise<void> {
+    this.groups = [];
+    for (const groupName of ["A", "B", "C", "D", "E", "F", "G", "H"]) {
+      const data = await this.getGroup(groupName);
+      this.groups.push({ name: groupName, teams: data.sort((a, b) => a.groupRanking - b.groupRanking) });
+    }
+    this.matches = [];
+    for (const groupName of ["A", "B", "C", "D", "E", "F", "G", "H"]) {
+      const data = await this.getMatches(groupName);
+      this.matches.push({ name: groupName, matches: data });
+    }
+  }
+
+  async getGroup(groupName: any): Promise<any[]> {
+    const data = await this.apiService.call("get", `Team/${groupName}`);
+    console.log(data.data);
+    return data.data;
+  }
+
+  async getMatches(groupName: any): Promise<any[]> {
+    const data = await this.apiService.call("get", `Match/${groupName}`);
+    console.log(data.data);
+    return data.data;
+  }
+
+  showMatches(groupName: any) {
+    this.selectedGroup = this.matches.find((group: { name: any; }) => group.name === groupName);
+    this.showGroupMatches = true;
+  }
+
+  hideMatches() {
+    this.showGroupMatches = false;
   }
 
 }
